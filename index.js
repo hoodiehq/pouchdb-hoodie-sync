a626e445eaf520edf08236c21a2ba29078a194df
zoepage <o.gasidlo@gmail.com>
2015-06-01 15:23:33 +0200

    feat(one): initial version

--- index.js
+++ index.js
@@ -1,15 +1,24 @@
 'use strict'
 
 var exports = module.exports = { hoodieSync: hoodieSync }
+var EventEmitter = require('events').EventEmitter
 
 function hoodieSync (options) {
-  var state = {}
+  var state = {
+    emitter: new EventEmitter()
+  }
+
   return {
     db: this,
     pull: require('./lib/pull').bind(this, state, options),
     push: require('./lib/push').bind(this, state, options),
     sync: require('./lib/sync').bind(this, state, options),
-    on: function () {}
+    connect: require('./lib/connect').bind(this, state, options),
+    disconnect: require('./lib/disconnect').bind(this, state, options),
+    isConnected: require('./lib/is-connected').bind(this, state, options),
+    on: require('./lib/on').bind(this, state, options),
+    off: require('./lib/off').bind(this, state, options),
+    one: require('./lib/one').bind(this, state, options)
   }
 }
 
