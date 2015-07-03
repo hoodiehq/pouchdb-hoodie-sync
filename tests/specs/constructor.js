386f48eacadb42880143f30fb429146362b42191
Nick Hehr <headhipster@hipsterbrown.com>
2015-07-03 16:02:56 -0400

    test(constructor): adds optional EventEmitter instance

--- tests/specs/constructor.js
+++ tests/specs/constructor.js
@@ -1,5 +1,6 @@
 var test = require('tape')
 var dbFactory = require('../utils/db')
+var EventEmitter = require('events').EventEmitter
 
 test('returns hoodie.store-inspired API', function (t) {
   t.plan(3)
@@ -12,4 +13,17 @@ test('returns hoodie.store-inspired API', function (t) {
 
   t.is(typeof store, 'object', 'is object')
   t.is(store.db, db, 'exposes db')
+})
+
+test('creates store with custom EventEmitter instance', function (t) {
+  t.plan(1)
+
+  var db = dbFactory()
+  var emitter = new EventEmitter()
+  var store = db.hoodieSync({emitter: emitter})
+
+  store.on('foo', function () {
+    t.ok('emitter used from options')
+  })
+  emitter.emit('foo')
 })
