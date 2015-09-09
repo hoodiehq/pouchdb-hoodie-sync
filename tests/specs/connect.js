acf5dadf7a3ef0ad51ca3279e6cece43a4b11628
Gregor Martynus <gregor@martynus.net>
2015-09-09 00:13:48 -0400

    test: api.connect to start sync with retry: true

--- tests/specs/connect.js
+++ tests/specs/connect.js
@@ -1,5 +1,8 @@
+var PouchDB = require('pouchdb')
 var test = require('tape')
+
 var dbFactory = require('../utils/db')
+var connect = require('../../lib/connect')
 
 /* create if db does not exist, ping if exists or created */
 test('api.connect()', function (t) {
@@ -10,4 +13,29 @@ test('api.connect()', function (t) {
   api.connect().then(function () {
     t.pass('connection is open')
   })
+})
+
+test('connect options', function (t) {
+  var state = {
+    emitter: {
+      emit: function () {}
+    }
+  }
+  var syncArgs
+  connect.bind({
+    constructor: PouchDB,
+    sync: function () {
+      syncArgs = arguments
+      return {
+        on: function () {}
+      }
+    }
+  })(state, {remote: 'remoteDb'})
+
+  t.is(syncArgs[0], 'remoteDb', 'calls api.sync with "remoteDb"')
+  t.is(syncArgs[1].live, true, 'calls api.sync with live: true')
+  t.is(syncArgs[1].retry, true, 'calls api.sync with retry: true')
+  t.is(syncArgs[1].create_target, true, 'calls api.sync with create_target: true')
+  t.is(syncArgs[1].since, 'now', 'calls api.sync with since: "now"')
+  t.end()
 })
